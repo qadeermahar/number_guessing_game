c9ea4eafb1582264c2875bef3a0b02bf19fe4cf7
qadeermahar <aqdmahar@gmail.com>
2024-03-26 03:32:24 +0500

    number_guessing_game

---
+++ main.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+
+
+const number = Math.floor(Math.random() * 10 + 1)
+
+let game = await inquirer.prompt([{
+
+    name: "guess_number",
+    type: "number",
+   message:"guess a number "
+
+}])
+
+if (game.guess_number=== number) {
+    console.log('"congrates"you guess write number.')
+}else{
+    console.log("invalid number")
+}
+
+
+
+
+
+
+
